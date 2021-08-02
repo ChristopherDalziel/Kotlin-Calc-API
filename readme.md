91755190b19d71301d3d3c1230c6b98a31ea7177
Chris Dalziel <christopher_dalziel@hotmail.com>
2021-08-02 14:45:59 +1000

    finished setup, added readme

---
+++ readme.md
@@ -0,0 +1,3 @@
+Calculator API - Kotlin <br /><br />
+start - `./gradlew run` <br /> 
+postman queries - <ul> add - `localhost:8080/add?a=10&b=10`
--- src/main/kotlin/to/dev/example/MainApp.kt
+++ src/main/kotlin/to/dev/example/MainApp.kt
@@ -1,9 +1,31 @@
 package to.dev.example
 
+import io.dropwizard.Application
 import io.dropwizard.Configuration
+import io.dropwizard.setup.Environment
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.QueryParam
 
 fun main(args: Array<String>) {
-    println("Hello World")
+    CalculatorApp().run(*args)
 }
 
 class CalculatorConfig(val name: String = "unknown") : Configuration()
+
+@Path("/")
+class CalculatorComponent {
+    @Path("/add")
+    @GET
+    fun add(@QueryParam("a") a: Double, @QueryParam("b") b: Double): Double {
+        return a + b
+    }
+}
+
+class CalculatorApp : Application<CalculatorConfig>() {
+    override fun run(configuration: CalculatorConfig, environment: Environment) {
+        println("Running ${configuration.name}")
+        val calculatorComponent = CalculatorComponent()
+        environment.jersey().register(calculatorComponent)
+    }
+}
