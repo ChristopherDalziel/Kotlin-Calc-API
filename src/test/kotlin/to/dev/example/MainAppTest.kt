fabe2d6d8876b27cd93631b02e127ad273ddf330
Chris Dalziel <christopher_dalziel@hotmail.com>
2021-08-02 16:48:20 +1000

    basic tests added for subtraction, multiplcation and division

--- src/test/kotlin/to/dev/example/MainAppTest.kt
+++ src/test/kotlin/to/dev/example/MainAppTest.kt
@@ -10,4 +10,22 @@ class MainAppTest {
        val result = CalculatorComponent().add(10.00, 10.00)
         assertThat(result).isEqualTo(10.00 + 10.00)
     }
+
+    @Test
+    fun`Given value a and value b, When SUBTRACT is called, Then a valueA is taken away from valueB`() {
+        val result = CalculatorComponent().subtract(10.00, 5.00)
+        assertThat(result).isEqualTo(10.00 - 5.00)
+    }
+
+    @Test
+    fun`Given value a and value b, When MULTIPLY is called, Then a valueA is taken away from valueB`() {
+        val result = CalculatorComponent().multiply(10.00, 10.00)
+        assertThat(result).isEqualTo(10.00 * 10.00)
+    }
+
+    @Test
+    fun`Given value a and value b, When DIVIDE is called, Then a valueA is taken away from valueB`() {
+        val result = CalculatorComponent().divide(5.00, 2.50)
+        assertThat(result).isEqualTo(5.00 / 2.50)
+    }
 }
