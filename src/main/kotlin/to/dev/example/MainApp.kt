ae75a9b5471f9a5406811c537749a5c0a18f25cb
Chris Dalziel <christopher_dalziel@hotmail.com>
2021-08-02 16:41:07 +1000

    division, multiplaction, subtraction added

--- src/main/kotlin/to/dev/example/MainApp.kt
+++ src/main/kotlin/to/dev/example/MainApp.kt
@@ -1,8 +1,8 @@
 package to.dev.example
 
 import io.dropwizard.Application
-import io.dropwizard.Configuration
 import io.dropwizard.setup.Environment
+import to.dev.example.configuration.CalculatorConfig
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.QueryParam
@@ -11,8 +11,6 @@ fun main(args: Array<String>) {
     CalculatorApp().run(*args)
 }
 
-class CalculatorConfig(val name: String = "unknown") : Configuration()
-
 @Path("/")
 class CalculatorComponent {
     @Path("/add")
@@ -20,6 +18,25 @@ class CalculatorComponent {
     fun add(@QueryParam("a") a: Double, @QueryParam("b") b: Double): Double {
         return a + b
     }
+
+    @Path("/subtract")
+    @GET
+    fun subtract(@QueryParam("a") a: Double, @QueryParam("b") b: Double): Double {
+        return a - b
+    }
+
+    @Path("/multiply")
+    @GET
+    fun multiply(@QueryParam("a") a: Double, @QueryParam("b") b: Double): Double {
+        return a * b
+    }
+
+    @Path("/divide")
+    @GET
+    fun divide(@QueryParam("a") a: Double, @QueryParam("b") b: Double): Double {
+        return a / b
+    }
+
 }
 
 class CalculatorApp : Application<CalculatorConfig>() {
